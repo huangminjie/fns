fa2dda34ce82a74227624282513876f40be0fed9
chenyt92 <732379072@qq.com>
2019-01-06 23:30:29 +0800

    init

---
+++ fns/Models/DB/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fns.Models.DB
+{
+    public partial class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Status { get; set; }
+        public DateTime? InsDt { get; set; }
+        public DateTime? UpdatedDt { get; set; }
+    }
+}
---
+++ fns/Models/DB/FinancialNewsContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace fns.Models.DB
+{
+    public partial class FinancialNewsContext : DbContext
+    {
+        public FinancialNewsContext()
+        {
+        }
+
+        public FinancialNewsContext(DbContextOptions<FinancialNewsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Admin> Admin { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=47.99.103.201,1433;Database=FinancialNews;User ID=admin;Password=123456a;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Admin>(entity =>
+            {
+                entity.ToTable("admin");
+
+                entity.Property(e => e.InsDt)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.UpdatedDt)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
