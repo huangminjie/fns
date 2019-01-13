7ad683af11b86ebd2f9014133ed48a1d024dccf7
chenyt92 <732379072@qq.com>
2019-01-13 16:13:52 +0800

    add tables

--- fns/Models/DB/FinancialNewsContext.cs
+++ fns/Models/DB/FinancialNewsContext.cs
@@ -16,6 +16,9 @@ namespace fns.Models.DB
         }
 
         public virtual DbSet<Admin> Admin { get; set; }
+        public virtual DbSet<Banner> Banner { get; set; }
+        public virtual DbSet<News> News { get; set; }
+        public virtual DbSet<User> User { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -32,30 +35,105 @@ namespace fns.Models.DB
 
             modelBuilder.Entity<Admin>(entity =>
             {
-                entity.ToTable("admin");
+                entity.Property(e => e.Id).HasColumnName("id");
 
                 entity.Property(e => e.InsDt)
+                    .HasColumnName("insDT")
                     .HasColumnType("datetime")
                     .HasDefaultValueSql("(getdate())");
 
                 entity.Property(e => e.Name)
                     .IsRequired()
+                    .HasColumnName("name")
                     .HasMaxLength(50);
 
                 entity.Property(e => e.Password)
                     .IsRequired()
+                    .HasColumnName("password")
                     .HasMaxLength(50);
 
                 entity.Property(e => e.Status)
                     .IsRequired()
+                    .HasColumnName("status")
                     .HasMaxLength(50);
 
                 entity.Property(e => e.UpdatedDt)
+                    .HasColumnName("updatedDt")
                     .HasColumnType("datetime")
                     .HasDefaultValueSql("(getdate())");
 
                 entity.Property(e => e.UserName)
                     .IsRequired()
+                    .HasColumnName("userName")
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Banner>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.LinkUrl)
+                    .IsRequired()
+                    .HasColumnName("linkUrl")
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.PicUrl)
+                    .IsRequired()
+                    .HasColumnName("picUrl")
+                    .HasMaxLength(500);
+            });
+
+            modelBuilder.Entity<News>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Content).HasColumnName("content");
+
+                entity.Property(e => e.DoRef).HasColumnName("doRef");
+
+                entity.Property(e => e.InsDt)
+                    .HasColumnName("insDT")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.NewsUrl)
+                    .IsRequired()
+                    .HasColumnName("newsUrl")
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnName("title")
+                    .HasMaxLength(500);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.InsDt)
+                    .HasColumnName("insDT")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasColumnName("status")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.UpdatedDt)
+                    .HasColumnName("updatedDt")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasColumnName("userName")
                     .HasMaxLength(50);
             });
         }
