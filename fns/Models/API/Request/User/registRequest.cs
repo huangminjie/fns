68c76f96c1705757522600c831d6700db44896d5
chenyt92 <732379072@qq.com>
2019-01-13 19:26:49 +0800

    修改字段名

--- fns/Models/API/Request/User/registRequest.cs
+++ fns/Models/API/Request/User/registRequest.cs
@@ -8,6 +8,6 @@ namespace fns.Models.API.Request.User
     public class registRequest
     {
         public string userName { get; set; }
-        public string passWord { get; set; }
+        public string password { get; set; }
     }
 }
