120e740f55a88e3173e25f73ce9282feb2e99f3a
Aghanim42 <756402018@qq.com>
2019-01-27 22:37:25 +0800

    add Splash

---
+++ fns/Controllers/SplashController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace fns.Controllers
+{
+    public class SplashController : Controller
+    {
+        public IActionResult Lists()
+        {
+            var list = new List<Models.Admin.VModels.vSplash>();
+            list.Add(new Models.Admin.VModels.vSplash() 
+            { 
+                Id = "1",
+                RedirectUrl = "www.baidu.com",
+                PicUrl = "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2230167403,4188800858&fm=27&gp=0.jpg",
+                Duration = 3
+            });
+            return PartialView(list);
+        }
+    }
+}
---
+++ fns/Models/Admin/VModels/vSplash.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace fns.Models.Admin.VModels
+{
+    public class vSplash
+    {
+        public string Id { get; set; }
+        public string PicUrl { get; set; }
+        public string RedirectUrl { get; set; }
+        public int Duration { get; set; }
+    }
+}
